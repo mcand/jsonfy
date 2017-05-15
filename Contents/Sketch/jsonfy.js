d6089777f2c8458336cc83bbc49b3764212e4e96
André Furquim <andreccomputacao@gmail.com>
2017-05-15 11:40:38 -0300

    Read JSON from generated in the filesystem

--- Contents/Sketch/jsonfy.js
+++ Contents/Sketch/jsonfy.js
@@ -1,58 +1,21 @@
-// Jsonfy, by André Furquim 
+// Jsonfy, by André Furquim
 //
-// This plugin exports  your project as JSON
+// This plugin exports your project as JSON
+
+@import "utils.js"
 
 function generateJson(context){
   var context = context.actionContext
 
   var documentPath = getDocumentPath(context)
   var jsonFileName = getJsonFileName(context)
-  var sketchFileName = getSketchFileName(context)
   var destinationPath = getDestination(context)
 
-  generateJsonFile(documentPath, destinationPath, jsonFileName)
-}
+  var jsonFilePath = generateJsonFile(documentPath, destinationPath, jsonFileName)
+  var sketchJson = readSketchGenaratedJson(jsonFilePath)
 
-// TODO: Put all the methods below in a utilitary file
+  log('Loga o Sketch Gerado Pelo JSON')
+  log(sketchJson);
 
-function generateJsonFile(documentPath, destinationPath, jsonFileName){
-  var command = "/usr/local/bin/sketchtool dump " + documentPath  + " > " + destinationPath + "" + jsonFileName
-  log("Executing command: " + command)
-  var args = ["-l", "-c", command]  
-  runCommand("/bin/bash", args)
-
-}
-
-function getDestination(context){
-  var documentPath = getDocumentPath(context)
-  var sketchFileName = getSketchFileName(context)
-  var regex = new RegExp(sketchFileName)
-  return documentPath.replace(regex, '')
-}
-
-function getSketchFileName(context){
-  return getFileNameInFormat(context, "sketch")
-}
-
-function getJsonFileName(context){
-  return getFileNameInFormat(context, "json")
-}
-
-function getFileNameInFormat(context, format) {
-  return context.document.displayName() + "." + format
-}
-
-function getDocumentPath(context){
-  var path = context.document.fileURL()
-  var documentPath = path + ""
-  return documentPath.replace(/file:[\/]{2}/i, '')
-}
-
-function runCommand(command, args){
-  var task = NSTask.alloc().init();
-  task.launchPath = command;
-  task.arguments = args;
-  task.launch();
-  task.waitUntilExit();
-  return (task.terminationStatus() == 0)
+  // Manda parsear essas coisa e pegar o que e interessante
 }
---
+++ Contents/Sketch/utils.js
@@ -0,0 +1,72 @@
+function generateJsonFile(documentPath, destinationPath, jsonFileName){
+  // We need to store this file in a file variable for us to read
+  var destinationFilePath = destinationPath + '' + jsonFileName
+  var command = '/usr/local/bin/sketchtool dump ' + documentPath  + ' > ' + destinationFilePath
+  log('Executing command: ' + command)
+  var args = ['-l', '-c', command]
+  runCommand('/bin/bash', args)
+  return destinationFilePath
+}
+
+function readSketchGenaratedJson(jsonFile){
+  var jsonFromSketch = loadData(jsonFile)
+  //var b = loadData("/Users/hero99/Desktop/pocbutton.json")
+  return jsonFromSketch
+}
+
+function getDocumentContext(context){
+  return context.document
+}
+
+function getDestination(context){
+  var documentPath = getDocumentPath(context)
+  var sketchFileName = getSketchFileName(context)
+  var regex = new RegExp(sketchFileName)
+  return documentPath.replace(regex, '')
+}
+
+function getSketchFileName(context){
+  return getFileNameInFormat(context, 'sketch')
+}
+
+function getJsonFileName(context){
+  return getFileNameInFormat(context, 'json')
+}
+
+function getFileNameInFormat(context, format) {
+  documentContext = getDocumentContext(context)
+  return documentContext.displayName() + '.' + format
+}
+
+function getDocumentPath(context){
+  var documentContext = getDocumentContext(context)
+  var path = documentContext.fileURL()
+  var documentPath = path + ''
+  return documentPath.replace(/file:[\/]{2}/i, '')
+}
+
+function runCommand(command, args){
+  var task = NSTask.alloc().init();
+  task.launchPath = command;
+  task.arguments = args;
+  task.launch();
+  task.waitUntilExit();
+  return (task.terminationStatus() == 0)
+}
+
+function readFileAsText(path) {
+  return [NSString stringWithContentsOfFile: path encoding: NSUTF8StringEncoding error: false];
+}
+
+function loadData(path) {
+  var contents = readFileAsText(path)
+  var data
+
+  try {
+    data = JSON.parse(contents);
+  } catch(e) {
+    context.document.showMessage("There was an error parsing data. Please make sure it's valid.")
+    return
+  }
+  return data
+}
